51ea42add9a6b42950a2fc974ec4327f754e197b
Felipe Torres <fgtorres18@gmail.com>
2016-12-21 09:55:05 +0100

    Updates of user interface
    This commit is about the changes on user interface for show new data
    aspects about Leishmania braziliensis anottation.

--- control/control-advsearch.php
+++ control/control-advsearch.php
@@ -1,29 +1,1 @@
-<?php
-
-include("../model/dao-search.php");
-include("../functions-aux.php");
-
-error_reporting(0);
-
-$advoption = $_POST["advoption"];
-
-if($advoption==1){
-    $genomeid =$_POST["genomeid"];
-    session_start();
-    $_SESSION["genes"] = SearchbyChromossome($conexao, $genomeid);
-    $_SESSION["ncrna"] = SearchbyChromossomencRNA($conexao, $genomeid);
-}
-if($advoption==2){
-    $ncrnatype =$_POST["ncrnatype"];
-    session_start();
-    $_SESSION["genes"] = array();
-    $_SESSION["ncrna"] = SearchbyncRNAtype($conexao, $ncrnatype);
-}
-if($advoption==3){
-
-    //echo"<script>document.location.href='http://regadb.bahia.fiocruz.br:4567'</script>";
-    echo "<script>window.alert('This mode is offline now. Try again more later...')</script>";
-    redireciona();
-}
-
-redireciona("search.php");
+<?phpinclude("../model/dao-search.php");include("../functions-aux.php");error_reporting(0);$advoption = $_POST["advoption"];if($advoption==1){    $genomeid =$_POST["genomeid"];    session_start();    $_SESSION["genes"] = SearchbyChromossome($conexao, $genomeid);    $_SESSION["ncrna"] = SearchbyChromossomencRNA($conexao, $genomeid);}if($advoption==2){    $ncrnatype =$_POST["ncrnatype"];    session_start();    $_SESSION["genes"] = array();    $_SESSION["ncrna"] = SearchbyncRNAtype($conexao, $ncrnatype);}if($advoption==3){        //echo"<script>document.location.href='http://regadb.bahia.fiocruz.br:4567'</script>";    echo "<script>window.alert('This mode is offline now. Try again more later...')</script>";    redireciona("leishdb.php");}redireciona("search.php");
--- view/fasta.php
+++ view/fasta.php
@@ -1,58 +1,1 @@
-<?php
-
-$geneid = $_GET["geneid"];
-$type = $_GET["type"];
-include("../model/dao-search.php");
-
-
-if ($type == "nc"){
-    $data = SearchbyncRNAID($conexao, $geneid); ?>
-    
-    <?php foreach ($data as $ncrna):
-        $sequence = getSequenceGene($conexao,$ncrna["genomeid"],$ncrna["end_location"],  $ncrna["start_location"]);
-        echo "\r\n > Gene {$ncrna["id"]} | {$ncrna["family"]} \r\n ";
-
-        $i = 0;
-        $tamanho = strlen($sequence["sequence"]);
-
-        while($i <= $tamanho){
-            echo substr($sequence["sequence"],$i,1);
-            $i += 1;         
-            if($i % 80 == 0){
-                echo  " \r\n ";
-            }    
-        }
-
-        header('Content-type: text/plain');
-        header( 'Content-Length: ' . strlen( $content ) ); 
-        header('Content-Disposition: attachment; filename="ncrna.fasta"');
-
-        echo $content;
-endforeach;
-}else{
-    $data = SearchbyGeneID($conexao, $geneid);  ?>
-    
-    <?php foreach ($data as $gene):
-        $sequence = getSequenceGene($conexao,$gene["genomeid"],$gene["endgenomelocal"],  $gene["startgenomelocal"]);
-        echo "\r\n > Gene {$gene["id"]} | {$gene["proteinname"]} \r\n ";
-        $i = 0;
-        $tamanho = strlen($sequence["sequence"]);
-
-        while($i <= $tamanho){
-            echo substr($sequence["sequence"],$i,1) ;
-            $i += 1;        
-            if($i % 80 == 0){
-                echo  " \r\n ";
-            }    
-        }
-
-        header('Content-type: text/plain');
-        header( 'Content-Length: ' . strlen( $content ) ); 
-        header('Content-Disposition: attachment; filename="gene.fasta"');
-
-        echo $content;
-    endforeach;
-}
-
-?>
-
+<?php$geneid = $_GET["geneid"];$type = $_GET["type"];include("../model/dao-search.php");if ($type == "nc"){    $data = SearchbyncRNAID($conexao, $geneid); ?>    <?php foreach ($data as $ncrna):        $sequence = getSequenceGene($conexao,$ncrna["chromosomeid"],$ncrna["end"],  $ncrna["start"]);        $sequence["sequence"] = trim($sequence["sequence"]);        $sequence["sequence"] = str_replace("\r\n","",$sequence["sequence"]);    echo "\r\n > Gene {$ncrna["id"]} | {$ncrna["type"]} | {$ncrna["family"]} \r\n ";        $i = 0;        $tamanho = strlen($sequence["sequence"]);        header('Content-type: text/plain');        header( 'Content-Length: ' . strlen( $content ) );        header('Content-Disposition: attachment; filename="LeishDB_ncrna_'. $geneid . '.fasta"');        while($i <= $tamanho){            echo substr($sequence["sequence"],$i,1);            $i += 1;                     if($i % 80 == 0){                echo  " \r\n ";            }            }        echo $content;endforeach;}else{    $data = SearchbyGeneID($conexao, $geneid);    foreach ($data as $gene):        $sequence = getSequenceGene($conexao,$gene["chromosomeid"],$gene["end"],  $gene["start"]);        $sequence["sequence"] = trim($sequence["sequence"]);        $sequence["sequence"] = str_replace("\r\n","",$sequence["sequence"]);        echo "\r\n > Gene {$gene["id"]} | {$gene["proteinname"]} \r\n ";        $i = 0;        $tamanho = strlen($sequence["sequence"]);        header('Content-type: text/plain');        header( 'Content-Length: ' . strlen( $content ) );        header('Content-Disposition: attachment; filename="LeishDB_gene_'. $geneid . '.fasta"');        while($i <= $tamanho){            echo substr($sequence["sequence"],$i,1) ;            $i += 1;                    if($i % 80 == 0){                echo  " \r\n ";            }            }        echo $content;    endforeach;}?>
