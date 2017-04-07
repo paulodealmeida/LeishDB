c61dc0ddfefc3d1763097ade62a181999791b624
Paulo de Almeida <paulodealmeida@gmail.com>
2017-04-07 08:05:55 -0300

    adicionado arquivo de configuração e script sql para criação do banco

---
+++ LeishDB_Structure.sql
@@ -0,0 +1,166 @@
+CREATE DATABASE  IF NOT EXISTS `leishdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `leishdb`;
+-- MySQL dump 10.13  Distrib 5.7.13, for linux-glibc2.5 (x86_64)
+--
+-- Host: localhost    Database: leishdb
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chromosomes`
+--
+
+DROP TABLE IF EXISTS `chromosomes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chromosomes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gbid` varchar(255) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `organismid` int(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `sequence` longtext,
+  `size` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_organism_chromossome` (`organismid`) USING BTREE,
+  CONSTRAINT `chromosomes_ibfk_1` FOREIGN KEY (`organismid`) REFERENCES `organisms` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `geneontology`
+--
+
+DROP TABLE IF EXISTS `geneontology`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `geneontology` (
+  `id` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `genes`
+--
+
+DROP TABLE IF EXISTS `genes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  `strand` varchar(255) DEFAULT NULL,
+  `chromosomeid` int(11) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `proteinid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ncrna`
+--
+
+DROP TABLE IF EXISTS `ncrna`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ncrna` (
+  `id` int(11) NOT NULL,
+  `chromosomeid` int(11) DEFAULT NULL,
+  `family` text,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  `type` text,
+  `predictor` text,
+  `covariancemodel` varchar(10) DEFAULT NULL,
+  `similarity` varchar(10) DEFAULT NULL,
+  `transcript` varchar(10) DEFAULT NULL,
+  `havetargetgenes` varchar(10) DEFAULT NULL,
+  `conservedspecies` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organisms`
+--
+
+DROP TABLE IF EXISTS `organisms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organisms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `organism` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `proteins`
+--
+
+DROP TABLE IF EXISTS `proteins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proteins` (
+  `id` varchar(255) NOT NULL,
+  `entryname` text,
+  `proteinname` text,
+  `genename` varchar(255) DEFAULT NULL,
+  `organism` varchar(255) DEFAULT NULL,
+  `taxonomiclineage` text,
+  `proteinfamily` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `proteinsgo`
+--
+
+DROP TABLE IF EXISTS `proteinsgo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proteinsgo` (
+  `proteinid` varchar(255) DEFAULT NULL,
+  `goid` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `publications`
+--
+
+DROP TABLE IF EXISTS `publications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publications` (
+  `proteinid` varchar(255) NOT NULL,
+  `pubmedid` varchar(255) NOT NULL,
+  PRIMARY KEY (`proteinid`,`pubmedid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- config.sh
+++ config.sh
@@ -3,9 +3,7 @@ PROJECT=leishdb
 URL=www.leishdb.local
 PASTA="$PWD"
 
-#TODO editar $config['base_url'] do arquivo application/config/config.php
 #TODO editar $db['default']['password'] do arquivo application/config/database.php
-
 
 aguardar(){
 	clear
@@ -26,8 +24,29 @@ instalar(){
 	if [ -d "vendor" ]; then
 		echo "Aplicação já foi instalada!"
 	else
-		#aguardar "Iniciando instalação"
-		#composer install
+#                MAINDB=leishdb
+
+                # If /root/.my.cnf exists then it won't ask for root password
+#                if [ -f /root/.my.cnf ]; then
+#
+#                    mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+#                    mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+#                    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+#                    mysql -e "FLUSH PRIVILEGES;"#
+
+                # If /root/.my.cnf doesn't exist then it'll ask for root password   
+#                else
+#                    echo "Please enter root user MySQL password!"
+#                    read rootpasswd
+#                    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+#                    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+#                    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+#                    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+#                fi
+
+                # Criação da estrutura do banco
+#                mysql -u user -p leishdb < db.sql
+
 		echo ""
 
 		if [ -f "/etc/apache2/sites-available/${PROJECT}.conf" ]; then
@@ -46,8 +65,8 @@ instalar(){
 		echo "        </Directory>" >> /etc/apache2/sites-available/${PROJECT}.conf
 		echo "        DocumentRoot \"${PASTA}\"" >> /etc/apache2/sites-available/${PROJECT}.conf
 		echo "        ServerAlias ${URL}" >> /etc/apache2/sites-available/${PROJECT}.conf
-		echo "        ErrorLog \"${PASTA}/logs/error.log\"" >> /etc/apache2/sites-available/${PROJECT}.conf
-		echo "        CustomLog \"${PASTA}/logs/access.log\" common" >> /etc/apache2/sites-available/${PROJECT}.conf
+		echo "        ErrorLog \"${PASTA}/application/logs/error.log\"" >> /etc/apache2/sites-available/${PROJECT}.conf
+		echo "        CustomLog \"${PASTA}/application/logs/access.log\" common" >> /etc/apache2/sites-available/${PROJECT}.conf
 		echo "</VirtualHost>" >> /etc/apache2/sites-available/${PROJECT}.conf
 
 		sudo echo "127.0.0.1	${URL}" >> /etc/hosts
