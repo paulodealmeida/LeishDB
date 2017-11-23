40a2d628687f6612c6d495e8ed486a63d8bbd091
Paulo de Almeida <paulodealmeida@gmail.com>
2017-11-23 18:30:35 -0300

    Removidos espaços vazios

--- application/public/js/main.js
+++ application/public/js/main.js
@@ -1,51 +1,46 @@
-
 function main() {
 
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+    (function () {
+        'use strict';
 
-	
-    // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 500;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
+        $('a.page-scroll').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        });
 
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    });
+        // Show Menu on Book
+        $(window).bind('scroll', function () {
+            var navHeight = $(window).height() - 500;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
 
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-	
-  	 
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        });
 
-}());
+        // Hide nav on click
+        $(".navbar-nav li a").click(function (event) {
+            // check if window is small enough so dropdown is created
+            var toggle = $(".navbar-toggle").is(":visible");
+            if (toggle) {
+                $(".navbar-collapse").collapse('hide');
+            }
+        });
 
+    }());
 
 }
 main();
