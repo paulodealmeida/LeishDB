723d828b3ece74a3e4ef64145cde18a5b57b9934
Paulo de Almeida <paulodealmeida@gmail.com>
2017-11-23 18:44:38 -0300

    corrigidos links do menu

--- application/resources/views/includes/menu.blade.php
+++ application/resources/views/includes/menu.blade.php
@@ -7,29 +7,29 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand page-scroll" href="#page-top">leishDB</a>
+        <a class="navbar-brand page-scroll" href="{!! url('/') !!}">leishDB</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
             <li>
-                <a href="#about" class="page-scroll">About</a>
+                <a href="{!! url('/#about') !!}" class="page-scroll">About</a>
             </li>
             <li>
-                <a href="#statistics" class="page-scroll">Statistics</a>
+                <a href="{!! url('/#statistics') !!}" class="page-scroll">Statistics</a>
             </li>
             <li>
-                <a href="#downloads" class="page-scroll">Downloads</a>
+                <a href="{!! url('/#downloads') !!}" class="page-scroll">Downloads</a>
             </li>
             <li>
-                <a href="#citing" class="page-scroll">Citing</a>
+                <a href="{!! url('/#citing') !!}" class="page-scroll">Citing</a>
             </li>
             <li>
-                <a href="mirna-graph" class="page-scroll">miRNA</a>
+                <a href="{!! url('/mirna-graph') !!}" class="page-scroll">miRNA</a>
             </li>
             <li>
-                <a href="#community" class="page-scroll">Community</a>
+                <a href="http://www.leishdb.com/community/" class="page-scroll">Community</a>
             </li>
             <li class="dropdown nav-item">
                 <a href="#tools" class="dropdown-toggle" data-toggle="dropdown">Tools
@@ -37,10 +37,10 @@
                 </a>
                 <ul class="dropdown-menu">
                     <li>
-                        <a href="http://www.leishdb.com/blast">BLAST</a>
+                        <a href="{!! url('/blast') !!}">BLAST</a>
                     </li>
                     <li>
-                        <a href="http://www.leishdb.com/browser">Genome browser</a>
+                        <a href="{!! url('/genome-browser') !!}">Genome browser</a>
                     </li>
                 </ul>
             </li>
