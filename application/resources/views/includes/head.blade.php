9258502220fa4b096ded3d2f9c4abf1ec4248dfc
Paulo de Almeida <paulodealmeida@gmail.com>
2017-11-23 18:52:29 -0300

    Adicionadas tags meta

--- application/resources/views/includes/head.blade.php
+++ application/resources/views/includes/head.blade.php
@@ -1,15 +1,28 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>LeishDB</title>
-<meta name="description" content="">
-<meta name="author" content="">
+<!-- Meta -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="http://www.leishdb.com/assets/images/Leishdb.jpg">
+<meta property="og:url" content="http://www.leishdb.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="LeishDB">
+<meta property="og:description" content="The leishDB is genomic annotation database of the genus Leishmania organisms. Data is stored in the genome of L. braziliensis . These data were the result of a master's work lasting two years.">
+<meta name="description" content="The leishDB is genomic annotation database of the genus Leishmania organisms. Data is stored in the genome of L. braziliensis . These data were the result of a master's work lasting two years.">
+<meta name="author" content="Felipe G. Torres">
+<meta name="keywords" content="leishmania braziliensis, genomic database, leishmania, genomic annotation, leishmaniasis, sequence,
+      DNA, genomic sequence, leishmaniasis patogen">
+<meta name="robots" content="index,follow">
+<meta property="og:title" content="LeishDB">
+<meta property="og:type" content="website">
+<link rel="shortcut icon" href="favicon.ico">
 
 <!-- Favicons
 ================================================== -->
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<!--<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
 <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">-->
 
 <!-- Bootstrap -->
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
@@ -23,8 +36,8 @@
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
 @yield('style')
